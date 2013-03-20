1da526eb459be1c85665eb073b50b22b37aceac1
Kyle <kyle@p00nMint.(none)>
2013-03-20 00:06:16 -0500

    First code push. Demo app is up and working. All functionality of validations lib demo is present and working.

---
+++ src/com/RichEditText/Demo/Validation/CiaoValidator.java
@@ -0,0 +1,24 @@
+package com.RichEditText.Demo.Validation;
+
+import android.text.TextUtils;
+import android.widget.EditText;
+
+import com.RichEditText.Validations.Validator;
+
+public class CiaoValidator
+    extends Validator
+{
+
+	public CiaoValidator( String customErrorMessage )
+	{
+		super( customErrorMessage );
+
+	}
+
+	@Override
+	public boolean isValid( EditText et )
+	{
+		return TextUtils.equals( et.getText(), "ciao" );
+	}
+
+}
---
+++ src/com/RichEditText/Demo/Widgets/LayoutListItem.java
@@ -0,0 +1,21 @@
+package com.RichEditText.Demo.Widgets;
+
+import android.content.Context;
+
+import com.RichEditText.Demo.Activities.LayoutExampleActivity;
+
+public class LayoutListItem extends ListItem {
+	private int layoutRes;
+	private int explanationString;
+
+	public LayoutListItem(String _listString, int _layoutRes, int _explanationStringRes) {
+		super(_listString);
+		layoutRes = _layoutRes;
+		explanationString = _explanationStringRes;
+	}
+
+	public void goToDemo(Context ctx) {
+		ctx.startActivity(LayoutExampleActivity.buildIntent(ctx, getListTitle(), layoutRes, explanationString));
+	}
+
+}
