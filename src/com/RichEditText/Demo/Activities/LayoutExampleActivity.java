a075274ffc5dda6790c678162d65cc112c088a8e
Kyle <kyle@p00nMint.(none)>
2013-03-20 04:18:20 -0500

    Implemented develoepr support for custom fonts

--- src/com/RichEditText/Demo/Activities/LayoutExampleActivity.java
+++ src/com/RichEditText/Demo/Activities/LayoutExampleActivity.java
@@ -1,10 +1,10 @@
 package com.RichEditText.Demo.Activities;
 
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
@@ -13,42 +13,65 @@ import android.widget.Toast;
 import com.RichEditText.Demo.R;
 import com.RichEditText.Widget.RichEditText;
 
-public class LayoutExampleActivity extends Activity {
-  private static final String EXTRA_LAYOUT_RES = "EXTRA_LAYOUT_RES";
-  private static final String EXTRA_LAYOUT_EXPL_STR_RES = "EXTRA_LAYOUT_EXPL_STR_RES";
-  private static final String EXTRA_TITLE = "EXTRA_TITLE";
+public class LayoutExampleActivity extends Activity{
+
+	private static final String	EXTRA_LAYOUT_RES	      = "EXTRA_LAYOUT_RES";
+	private static final String	EXTRA_LAYOUT_EXPL_STR_RES	=
+	                                                          "EXTRA_LAYOUT_EXPL_STR_RES";
+	private static final String	EXTRA_TITLE	              = "EXTRA_TITLE";
 
 
-  private FrameLayout flContainer;
-  private TextView tvExplanation;
-  private TextView tvTitle;
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.layout_examplegeneric);
+	private FrameLayout	        flContainer;
+	private TextView	        tvExplanation;
+	private TextView	        tvTitle;
 
-    flContainer     = (FrameLayout) findViewById(R.id.fl);
-    tvExplanation   = (TextView)    findViewById(R.id.tv_explanation);
-    tvTitle         = (TextView)    findViewById(R.id.tv_title);
-    
-    flContainer.addView(LayoutInflater.from(this).inflate(getIntent().getIntExtra(EXTRA_LAYOUT_RES, 0), flContainer, false));
-    tvExplanation.setText(getIntent().getIntExtra(EXTRA_LAYOUT_EXPL_STR_RES, 0));
-    tvTitle.setText(getIntent().getStringExtra(EXTRA_TITLE));
-  }
 
-  public static Intent buildIntent(Context ctx, String title, int layoutRes, int explanationString) {
-    Intent toRet = new Intent(ctx, LayoutExampleActivity.class);
-    toRet.putExtra(EXTRA_TITLE, title);
-    toRet.putExtra(EXTRA_LAYOUT_RES, layoutRes);
-    toRet.putExtra(EXTRA_LAYOUT_EXPL_STR_RES, explanationString);
-    return toRet;
-  }
-  
-  public void onClickValidate(View v) {
-	  RichEditText fdt = (RichEditText) findViewById(R.id.et);
-    if (fdt.testValidity()) {
-      Toast.makeText(this, ":)", Toast.LENGTH_LONG).show();
-    } 
-  }
-  
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_examplegeneric);
+
+		flContainer = (FrameLayout) findViewById(R.id.fl);
+		tvExplanation = (TextView) findViewById(R.id.tv_explanation);
+		tvTitle = (TextView) findViewById(R.id.tv_title);
+
+		flContainer.addView(getLayoutInflater().inflate(getIntent().getIntExtra(EXTRA_LAYOUT_RES,
+		                                                                        0),
+		                                                flContainer,
+		                                                false));
+		tvExplanation.setText(getIntent().getIntExtra(EXTRA_LAYOUT_EXPL_STR_RES,
+		                                              0));
+		tvTitle.setText(getIntent().getStringExtra(EXTRA_TITLE));
+	}
+
+
+	public static Intent buildIntent(Context ctx,
+	                                 String title,
+	                                 int layoutRes,
+	                                 int explanationString){
+
+		Intent toRet = new Intent(ctx,
+		                          LayoutExampleActivity.class);
+		toRet.putExtra(EXTRA_TITLE,
+		               title);
+		toRet.putExtra(EXTRA_LAYOUT_RES,
+		               layoutRes);
+		toRet.putExtra(EXTRA_LAYOUT_EXPL_STR_RES,
+		               explanationString);
+		return toRet;
+	}
+
+
+	public void onClickValidate(View v){
+
+		RichEditText fdt = (RichEditText) findViewById(R.id.et);
+		if (fdt.testValidity()){
+			Toast.makeText(this,
+			               ":)",
+			               Toast.LENGTH_LONG)
+			     .show();
+		}
+	}
+
 }
